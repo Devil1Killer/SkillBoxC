0f1f9221ed198f6fbae94fad9411ca7661e9859f
Devil1Killer <120336372+Devil1Killer@users.noreply.github.com>
2024-03-01 17:08:16 +0600

    .

--- SkillBox/SkillBox.cpp
+++ SkillBox/SkillBox.cpp
@@ -6,12 +6,15 @@ int main()
 
     double a = 10, b = 10;;
 
+    std::cout << "Введите первое число: ";
     std::cin >> a;
+
+    std::cout << "Введите второе число: ";
     std::cin >> b;
 
     SquareNumber(a, b);
 
-    std::cout << SquareNumber(a, b);
+    std::cout << "Ответ: " << SquareNumber(a, b);
 
 }
 
