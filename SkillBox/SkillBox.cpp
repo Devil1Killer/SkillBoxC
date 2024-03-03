f6789fec67531dac5644b0ed1c02f742dc266e47
Devil1Killer <120336372+Devil1Killer@users.noreply.github.com>
2024-03-04 04:39:04 +0500

    14.4 Практическая работа + 15.4 Практическая работа

--- SkillBox/SkillBox.cpp
+++ SkillBox/SkillBox.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
 #include <string>
 
+void ParityAlgorithm(int number, bool key) {
+
+    for (int i = key ? 1 : 0; i <= number; i += 2) {
+
+        std::cout << i << " ";
+
+    }
+
+    std::cout << std::endl;
+
+}
+
 int main()
 {
 
@@ -22,6 +34,26 @@ int main()
     std::cout << text[0] << std::endl;
     std::cout << text[text.size() - 1] << std::endl;
      
+    // 15.4 Практическая работа
+
+    int number;
+
+    std::cout << "Введите число: ";
+    std::cin >> number;
+
+    std::cout << (!(number % 2) ? "Чётные: " : "Нечётные: ");
+    ParityAlgorithm(number, number % 2);
+
+
+
+
+
+    //for (int i = 0; i < number; i++) {
+
+    //    std::cout << (!(i % 2) ? "Чётные: " : "Нечётные: ") << i << std::endl;
+
+    //}
+
 
 }
 
