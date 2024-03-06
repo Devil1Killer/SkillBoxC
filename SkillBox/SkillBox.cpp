0085a62827c27a12b89cfb2cf53cae1154414f5a
Devil1Killer <120336372+Devil1Killer@users.noreply.github.com>
2024-03-06 22:00:31 +0500

    17.5 Практическая работа+ 18.5

--- SkillBox/SkillBox.cpp
+++ SkillBox/SkillBox.cpp
@@ -1,6 +1,87 @@
 ﻿#include <iostream>
 #include <string>
 #include <time.h>
+#include "algorithm"
+
+class Player {
+
+public:
+
+    std::string Name;
+    int Score;
+
+    void SetData(std::string Name, int Score) {
+
+        this->Name = Name;
+        this->Score = Score;
+
+    }
+
+};
+
+void FormationLeaderboard(int CountPlayer, Player *ArrayPlayers) {
+
+    std::string Name;
+    int Score;
+
+    for (int i = 0; i < CountPlayer; i++) {
+
+        std::cout << "Введите имя " << i + 1 << " игрока: ";
+
+        std::cin >> Name;
+
+        std::cout << "Введите очки " << i + 1 << " игрока: ";
+
+        std::cin >> Score;
+
+        ArrayPlayers[i].SetData(Name, Score);
+
+    }
+
+}
+
+void DisplayLeaderboard(int CountPlayer, Player* ArrayPlayers) {
+
+    /*
+    
+    // Пользоваться контейнерами стандартной библиотеки, такими как std::vector и подобными, запрещено.
+    // Если принципиально не нужно использовать библиотеки #include "algorithm"
+
+    Player temp;
+
+    for (int i = 0; i < CountPlayer; i++) {
+
+        for (int j = i + 1; j < CountPlayer; j++) {
+
+            if (ArrayPlayers[i].Score < ArrayPlayers[j].Score) {
+
+                temp = ArrayPlayers[i];
+                ArrayPlayers[i] = ArrayPlayers[j];
+                ArrayPlayers[j] = temp;
+
+            }
+
+        }
+
+    }
+
+    */
+    
+    std::sort(ArrayPlayers, ArrayPlayers + CountPlayer, [](const Player& p1, const Player& p2) {
+
+        return p1.Score > p2.Score;
+
+    });
+
+    for (int i = 0; i < CountPlayer; i++) {
+
+        std::cout << "Имя игрока: " << ArrayPlayers[i].Name << " его очки: " << ArrayPlayers[i].Score << std::endl;
+
+    }
+
+}
+
+//--------------------
 
 class Vector {
 
@@ -12,13 +93,7 @@ private:
 
 public:
 
-    Vector(double x, double y, double z) {
-
-        this->x = x;
-        this->y = y;
-        this->z = z;
-
-    }
+    Vector(double x, double y, double z) : x(x), y(y), z(z) {};
 
     void showValues() {
 
@@ -36,51 +111,33 @@ public:
 
 int main() {
 
-    std::cout << "16.5 Практическая работа." << std::endl;
-
-    const int a = 5, b = 5;
-
-    struct tm buf;
-    time_t t = time(NULL);
-    localtime_s(&buf, &t);
-
-    int arrey[a][b];
-
-    for (int i = 0; i < a; i++) {
-
-        for (int j = 0; j < b; j++) {
-
-            arrey[i][j] = i + j;
-            std::cout << i + j << " ";
-
-        }
-
-        std::cout << std::endl;
-
-    }
-
-    int temp = 0;
-
-    for (int i = 0; i < b; i++) {
-
-        temp += arrey[(buf.tm_mday % a) - 1][i];
-
-    }
-
-    std::cout << std::endl;
-
-    std::cout << temp;
-
-    std::cout << std::endl << std::endl;
-
-    //17.5 Практическая работа ------------------------------------------
-
     std::cout << "17.5 Практическая работа." << std::endl;
 
     Vector v(3, 4, 5);
 
     v.showValues();
+
     std::cout << "Длина модуля вектора: " << v.getLength() << std::endl;
+
+    std::cout << std::endl << std::endl;
+
+    //18.5 Практическая работа ------------------------------------------
+
+    std::cout << "18.5 Практическая работа." << std::endl;
+
+    int CountPlayer;
+
+    std::cout << "Введите количество игроков: ";
+
+    std::cin >> CountPlayer;
+
+    Player* ArrayPlayers = new Player[CountPlayer];
+
+    FormationLeaderboard(CountPlayer, ArrayPlayers);
+
+    DisplayLeaderboard(CountPlayer, ArrayPlayers);
+
+    delete[] ArrayPlayers;
 
 }
 
