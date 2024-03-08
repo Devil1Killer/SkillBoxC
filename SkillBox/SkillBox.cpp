229af28307d1ee1c39e619060e1156adb1ad1963
Devil1Killer <120336372+Devil1Killer@users.noreply.github.com>
2024-03-08 22:22:41 +0500

    19.5 Практическая работа

--- SkillBox/SkillBox.cpp
+++ SkillBox/SkillBox.cpp
@@ -1,7 +1,4 @@
 ﻿#include <iostream>
-#include <string>
-#include "algorithm"
-#include <vector>
 
 class Animal {
 
@@ -51,107 +48,7 @@ public:
 
 };
 
-//--------------------------------------------------
-
-class Player {
-
-public:
-
-    std::string Name;
-    int Score;
-
-    void SetData(std::string Name, int Score) {
-
-        this->Name = Name;
-        this->Score = Score;
-
-    }
-
-};
-
-void FormationLeaderboard(int CountPlayer, Player *ArrayPlayers) {
-
-    std::string Name;
-    int Score;
-
-    for (int i = 0; i < CountPlayer; i++) {
-
-        std::cout << "Введите имя " << i + 1 << " игрока: ";
-
-        std::cin >> Name;
-
-        std::cout << "Введите очки " << i + 1 << " игрока: ";
-
-        std::cin >> Score;
-
-        ArrayPlayers[i].SetData(Name, Score);
-
-    }
-
-}
-
-void DisplayLeaderboard(int CountPlayer, Player* ArrayPlayers) {
-
-    /*
-    
-    // Пользоваться контейнерами стандартной библиотеки, такими как std::vector и подобными, запрещено.
-    // Если принципиально не нужно использовать библиотеки #include "algorithm"
-
-    Player temp;
-
-    for (int i = 0; i < CountPlayer; i++) {
-
-        for (int j = i + 1; j < CountPlayer; j++) {
-
-            if (ArrayPlayers[i].Score < ArrayPlayers[j].Score) {
-
-                temp = ArrayPlayers[i];
-                ArrayPlayers[i] = ArrayPlayers[j];
-                ArrayPlayers[j] = temp;
-
-            }
-
-        }
-
-    }
-
-    */
-    
-    std::sort(ArrayPlayers, ArrayPlayers + CountPlayer, [](const Player& p1, const Player& p2) {
-
-        return p1.Score > p2.Score;
-
-    });
-
-    for (int i = 0; i < CountPlayer; i++) {
-
-        std::cout << "Имя игрока: " << ArrayPlayers[i].Name << " его очки: " << ArrayPlayers[i].Score << std::endl;
-
-    }
-
-}
-
 int main() {
-
-    std::cout << "18.5 Практическая работа." << std::endl;
-
-    int CountPlayer;
-
-    std::cout << "Введите количество игроков: ";
-
-    std::cin >> CountPlayer;
-
-    Player* ArrayPlayers = new Player[CountPlayer];
-
-    FormationLeaderboard(CountPlayer, ArrayPlayers);
-
-    DisplayLeaderboard(CountPlayer, ArrayPlayers);
-
-    delete[] ArrayPlayers;
-
-    std::cout << std::endl << std::endl;
-
-    //19.5 Практическая работа ------------------------------------------
 
     std::cout << "19.5 Практическая работа." << std::endl;
 
@@ -167,7 +64,11 @@ int main() {
 
     }
 
-    delete[] animals;
+    for (Animal* animal : animals) {
+
+        delete animal;
+
+    }
 
 }
 
